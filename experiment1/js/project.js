9965a483d21e63eb870325a37e9924c8fb185305
RubyTseng2538 <86134957+RubyTseng2538@users.noreply.github.com>
2024-04-05 13:49:08 -0700

    experiment one complete

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -1,34 +1,62 @@
-// project.js - purpose and description here
-// Author: Your Name
-// Date:
-
-// NOTE: This is how we might start a basic JavaaScript OOP project
-
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-
-// define a class
-class MyProjectClass {
-  // constructor function
-  constructor(param1, param2) {
-    // set properties using 'this' keyword
-    this.property1 = param1;
-    this.property2 = param2;
+const fillers = {
+  pre1:["Mystic", "Celestial", "Arcane", "Ethereal", "Nature's", "Bewitching", "Serene", "Enchanted"],
+  pre2:["Mystic", "Celestial", "Arcane", "Ethereal", "Nature's", "Bewitching", "Serene", "Enchanted"],
+  post:["Elixir", "Aura", "Essence", "Dream", "Enchantment", "Bloom", "Harmony", "Magic", "Fragrance", "Mist", "Potion"],
+  adjective:["floral", "spicy", "woody", "citrusy", "fresh", "aromatic", "fruity", "sweet", "powdery", "exotic", "seductive", "sensual", "elegant", "romantic", "bold", "euphoric", "hypnotic", "bewitching", "captivating", "dreamy", "mesmerizing", "alluring"],
+  adjective2:["floral", "spicy", "woody", "citrusy", "fresh", "aromatic", "fruity", "sweet", "powdery", "exotic", "seductive", "sensual", "elegant", "romantic", "bold", "euphoric", "hypnotic", "bewitching", "captivating", "dreamy", "mesmerizing", "alluring"],
+  adjective3:["floral", "spicy", "woody", "citrusy", "fresh", "aromatic", "fruity", "sweet", "powdery", "exotic", "seductive", "sensual", "elegant", "romantic", "bold", "euphoric", "hypnotic", "bewitching", "captivating", "dreamy", "mesmerizing", "alluring"],
+  adjective4:["floral", "spicy", "woody", "citrusy", "fresh", "aromatic", "fruity", "sweet", "powdery", "exotic", "seductive", "sensual", "elegant", "romantic", "bold", "euphoric", "hypnotic", "bewitching", "captivating", "dreamy", "mesmerizing", "alluring"],
+  adjective5:["floral", "spicy", "woody", "citrusy", "fresh", "aromatic", "fruity", "sweet", "powdery", "exotic", "seductive", "sensual", "elegant", "romantic", "bold", "euphoric", "hypnotic", "bewitching", "captivating", "dreamy", "mesmerizing", "alluring"],
+  noun:["Alchemy", "Apothecary", "Aromas", "Scentcraft", "Perfumery", "Parfums", "Scents", "Fragrances"],
+  location:["Aetheria", "Elvenwood", "Dwarvenhold", "Merfolk Dominion", "Feywild Realm", "Dragonscale Empire", "Nomadic Tribes of the Sandsea", "Sylvan Grove", "Shadowlands", "Celestia"],
+  scent1:["Starlight Blossom", "Moonvine", "Dragonleaf", "Faerie Fern", "Unicorn Lily", "Phoenix Ember", "Mermaid Seaweed", "Elfwood Moss", "Pixie Petal", "Nymph Nectar", "Darkwood Bark", "Serpent Vine", "Griffin Grass"],
+  scent2:["Starlight Blossom", "Moonvine", "Dragonleaf", "Faerie Fern", "Unicorn Lily", "Phoenix Ember", "Mermaid Seaweed", "Elfwood Moss", "Pixie Petal", "Nymph Nectar", "Darkwood Bark", "Serpent Vine", "Griffin Grass"],
+  scent3:["Starlight Blossom", "Moonvine", "Dragonleaf", "Faerie Fern", "Unicorn Lily", "Phoenix Ember", "Mermaid Seaweed", "Elfwood Moss", "Pixie Petal", "Nymph Nectar", "Darkwood Bark", "Serpent Vine", "Griffin Grass"]
+  };
+  
+  const template = `Perfume Name: $pre1 $post
+  \nPerfumer/Brand: $pre2 $noun
+  \nDescription: This is a $adjective $adjective2 fragrance inspired by $location. Crafted from $adjective3 $scent1, this perfume blends seamlessly with $adjective4 $scent2 and $adjective5 $scent3, culminating in a scent that captures the essence of $location. 
+  \nKey Notes: $scent1, $scent2, and $scent3
+  `;
+  
+  // STUDENTS: You don't need to edit code below this line.
+  
+  const slotPattern = /\$(\w+)/;
+  
+  function replacer(match, name) {
+    let options = fillers[name];
+    if (options) {
+      return options[Math.floor(Math.random() * options.length)];
+    } else {
+      return `<UNKNOWN:${name}>`;
+    }
   }
   
-  // define a method
-  myMethod() {
-    // code to run when method is called
+  function generate() {
+    let story = template;
+    let usedFillers = {}; // Keep track of used fillers
+    while (story.match(slotPattern)) {
+      story = story.replace(slotPattern, (match, name) => {
+        // If filler for this slot has not been used yet, pick one and mark it as used
+        if (!usedFillers[name]) {
+          let options = fillers[name];
+          if (options) {
+            let selectedOption = options[Math.floor(Math.random() * options.length)];
+            usedFillers[name] = selectedOption;
+            return selectedOption;
+          }
+        }
+        // If filler has been used, stick to the same one
+        return usedFillers[name];
+      });
+    }
+  
+    /* global box */
+    $("#box").text(story);
   }
-}
-
-function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
-
-  // call a method on the instance
-  myInstance.myMethod();
-}
-
-// let's get this party started - uncomment me
-//main();
+  
+  /* global clicker */
+  $("#clicker").click(generate);
+  
+  generate();
